6e0b85c217e02a91aa5efa125979c8b0b44e36d8
Tran Tam <tam.tran@employmenthero.com>
2018-09-26 17:36:00 +0000

    add date for PROMPT, add install_dep script

---
+++ install_deps.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# insstall oh-my-tmux: gpakosz/.tmux
+git clone https://github.com/gpakosz/.tmux.git ~/
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+# install tpm for tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# zsh-plugins
+git clone https://github.com/popstas/zsh-command-time.git ~/.oh-my-zsh/custom/plugins/command-time
+
