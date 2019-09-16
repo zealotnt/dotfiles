9ab0a525eaeeabaa37a2d16a5994f2c7e761cbbd
Tran Tam <tam.tran@employmenthero.com>
2019-09-16 18:38:13 +0700

    update dconf-editor

--- dconf/export.sh
+++ dconf/export.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# to restore, read from this
+# https://github.com/linuxmint/Cinnamon/wiki/Backing-up-and-restoring-your-cinnamon-settings-(dconf)
 dconf dump / > dconf.dump.conf
+# or (untested)
+# dconf load / < dconf.dump.conf
+
+# 2nd method to backup, I will save all contents of this file
 gsettings list-recursively > gsettings.list-recursively.conf
 
