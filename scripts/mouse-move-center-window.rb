33d0a4e9e4337aab6f3591ed0fe522290d68e97b
Tran Tam <tam.tran@employmenthero.com>
2019-11-11 15:16:14 +0700

    add move-mouse to center of current active windows

---
+++ scripts/mouse-move-center-window.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'pry-byebug'
+
+# to debug, just type `xwininfo`, then select the window by mouse
+# the info will be printed, then we will have id of windows from the result
+activeWindowInfo = `xwininfo -id $(xdotool getactivewindow)`
+
+# not so useful, deprecated
+# winPosStr = activeWindowInfo.match(/\-geometry (.*)/)[1]
+# res = winPosStr.match(/(\d+)x(\d+)[\+\-](\d+)[\+\-](\d+)/)
+# sizeX, sizeY = res[1], res[2]
+
+winPosStr = activeWindowInfo.match /Corners:\s+\+(\d+)\+(\d+)\s+.*/
+sizeX = activeWindowInfo.match(/Width: (\d+)/)[1]
+sizeY = activeWindowInfo.match(/Height: (\d+)/)[1]
+
+posX, posY = winPosStr[1], winPosStr[2]
+centerPointX = posX.to_i+sizeX.to_i/2
+centerPointY = posY.to_i+sizeY.to_i/2
+
+`xdotool mousemove #{centerPointX} #{centerPointY}`
