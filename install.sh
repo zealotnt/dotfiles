f771bac9a1344b065aecf8c60172df4cdc8531ad
Tran Tam <tam.tran@employmenthero.com>
2019-12-11 10:40:52 +0700

    avoid creating ~ folder

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@
 . ~/dotfiles/my-dot-files.sh
 
 BACKUP_FOLDER="~/dotfiles/backup/$(date +"%Y%m%d")-$(date +"%H%M%S")"
-mkdir -p "$BACKUP_FOLDER"
+mkdir -p $BACKUP_FOLDER
 echo "Creating backup folder $BACKUP_FOLDER"
 
 for file in  "${MYDOTFILES[@]}"
 do
 	echo "Backing up $file"
-	mv ~/$file "$BACKUP_FOLDER/$file.bak"
+	mv ~/$file $BACKUP_FOLDER/$file.bak
 	ln -sf $(realpath $file) ~/
 done
