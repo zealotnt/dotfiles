6f732c3c29276654ecdf7c37488d86d1ab6e1809
Tran Tam <tam.tran@employmenthero.com>
2020-07-07 17:32:48 +0700

    config touchpad on i3

--- install_dotfiles_w_deps.sh
+++ install_dotfiles_w_deps.sh
@@ -108,3 +108,16 @@ sudo snap install hub --classic
 # user related things
 sudo usermod -aG bluetooth pulse
 
+# config touchpad
+# ref https://cravencode.com/post/essentials/enable-tap-to-click-in-i3wm/
+sudo mkdir -p /etc/X11/xorg.conf.d && sudo tee <<'EOF' /etc/X11/xorg.conf.d/90-touchpad.conf 1> /dev/null
+Section "InputClass"
+        Identifier "touchpad"
+        MatchIsTouchpad "on"
+        Driver "libinput"
+        Option "Tapping" "on"
+        Option "TappingButtonMap" "lrm"
+        Option "ScrollMethod" "twofinger"
+EndSection
+EOF
+
