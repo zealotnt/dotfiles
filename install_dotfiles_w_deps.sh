7f2b06a60f227d943c23239da0f3daccc94fd494
Tran Tam <tam.tran@employmenthero.com>
2020-07-10 15:33:02 +0700

    add deps for setup new vps

--- install_dotfiles_w_deps.sh
+++ install_dotfiles_w_deps.sh
@@ -54,7 +54,8 @@ sudo apt install -y moreutils jq tmux google-chrome-stable emacs26 rofi ruby \
      python-docutils python3-docutils `# requires for arandr` \
      libavcodec-dev pulseaudio-module-bluetooth `# install bluetooth aptx,etc...` \
      system-config-samba samba samba-common-bin cmake grsync libtool libnemo-extension-dev \
-     yarn socat libsqlite3-dev dialog guvcview ethtool ofono ofono-phonesim ofono-phonesim-autostart
+     yarn socat libsqlite3-dev dialog guvcview ethtool ofono ofono-phonesim ofono-phonesim-autostart \
+     libpq-dev
 
 ############################################################################
 # install editor tools so that we can effectively follow fresh_install.sh
