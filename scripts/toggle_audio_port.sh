abce25c72b42fe83a2b3d2a7f81dfb65f7dcaba1
Tran Tam <tam.tran@employmenthero.com>
2020-11-29 11:27:54 +0700

    add toggle_audio_port.sh function
    add default volume blocklet

---
+++ scripts/toggle_audio_port.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CURRENT_PORT=$(pacmd list-sinks | grep 'active port' | grep 'analog' | sed -r -n 's/.*<(.*)>/\1/p')
+if [[ "$CURRENT_PORT" == "analog-output-headphones" ]]; then
+  pacmd set-sink-port 0 analog-output-lineout
+  echo "🔊" > ~/.local/audio_device
+else
+  pacmd set-sink-port 0 analog-output-headphones
+  echo "🔊" > ~/.local/audio_device
+fi
+
