8c2022d118385220b7aec0c7c054f994fedfd8fd
Tran Tam <tam.tran@employmenthero.com>
2021-03-02 20:54:03 +0700

    add air-con comfort/fan mode for blackbeancontrol

--- scripts/toggle_air_con.sh
+++ scripts/toggle_air_con.sh
@@ -14,6 +14,18 @@ else
   if [[ "$1" == "0" ]]; then
     python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOffAirCon
     echo 0 > ~/.local_air_con
+  elif  [[ "$1" == "27" ]]; then
+    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConComfort27
+    echo 1 > ~/.local_air_con
+  elif  [[ "$1" == "28" ]]; then
+    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConComfort28
+    echo 1 > ~/.local_air_con
+  elif  [[ "$1" == "29" ]]; then
+    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConComfort29
+    echo 1 > ~/.local_air_con
+  elif  [[ "$1" == "fan" ]]; then
+    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConFanOnly
+    echo 1 > ~/.local_air_con
   else
     python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirCon
     echo 1 > ~/.local_air_con
