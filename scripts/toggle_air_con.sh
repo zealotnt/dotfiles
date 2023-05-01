81ed99b7c8771255b3dee9c883a0c11f221191e2
Tran Tam <tam.tran@employmenthero.com>
2023-05-01 20:54:36 +0700

    update script for blaclbeancontrol

--- scripts/toggle_air_con.sh
+++ scripts/toggle_air_con.sh
@@ -1,41 +1,43 @@
 #!/usr/bin/bash
 
+BLACKBEAN_CONTROL_PATH=/home/zealot/workspace_misc/rm3_mini_controller
+#BLACKBEAN_CONTROL_PATH=/root/dev/rm3_mini_controller
 
 if [[ $# == 0 ]]; then
   LOCAL_AIR_CON=$(cat ~/.local_air_con)
   if [[ "$LOCAL_AIR_CON" == "0" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirCon
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnAirCon
     echo 1 > ~/.local_air_con
   else
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOffAirCon
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOffAirCon
     echo 0 > ~/.local_air_con
   fi
 else
   if [[ "$1" == "0" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOffAirCon
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOffAirCon
     echo 0 > ~/.local_air_con
   elif  [[ "$1" == "t26OldDaikin" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c ToggleAirConOldDaikin26
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c ToggleAirConOldDaikin26
   elif  [[ "$1" == "26OldDaikin" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c SetAirConOldDaikin26
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c SetAirConOldDaikin26
   elif  [[ "$1" == "27OldDaikin" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c SetAirConOldDaikin27
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c SetAirConOldDaikin27
   elif  [[ "$1" == "28OldDaikin" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c SetAirConOldDaikin28
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c SetAirConOldDaikin28
   elif  [[ "$1" == "27" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConComfort27
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnAirConComfort27
     echo 1 > ~/.local_air_con
   elif  [[ "$1" == "28" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConComfort28
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnAirConComfort28
     echo 1 > ~/.local_air_con
   elif  [[ "$1" == "29" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConComfort29
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnAirConComfort29
     echo 1 > ~/.local_air_con
   elif  [[ "$1" == "fan" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirConFanOnly
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnAirConFanOnly
     echo 1 > ~/.local_air_con
   else
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnAirCon
+    python  $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnAirCon
     echo 1 > ~/.local_air_con
   fi
 fi
--- scripts/toggle_fan.sh
+++ scripts/toggle_fan.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/bash
 
+BLACKBEAN_CONTROL_PATH=/home/zealot/workspace_misc/rm3_mini_controller
+#BLACKBEAN_CONTROL_PATH=/root/dev/rm3_mini_controller
+
 if [[ $# == 0 ]]; then
   LOCAL_FAN=$(cat ~/.local_fan)
   if [[ "$LOCAL_FAN" == "0" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnFan
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnFan
     echo 1 > ~/.local_fan
   else
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOffFan
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOffFan
     echo 0 > ~/.local_fan
   fi
 else
   if [[ "$1" == "0" ]]; then
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOffFan
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOffFan
     echo 0 > ~/.local_fan
   else
-    python /home/zealot/workspace_misc/rm3_mini_controller/BlackBeanControl.py -c TurnOnFan
+    python $BLACKBEAN_CONTROL_PATH/BlackBeanControl.py -c TurnOnFan
     echo 1 > ~/.local_fan
   fi
 fi
