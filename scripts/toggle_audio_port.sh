643715d912f7f9d5bcae3bbcdd054baae479d8eb
zealot <tranminhtam.10192@gmail.com>
2024-04-11 23:25:09 +0700

    update for cmp

--- scripts/toggle_audio_port.sh
+++ scripts/toggle_audio_port.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 CURRENT_PORT=$(pacmd list-sinks | grep 'active port' | grep 'analog' | sed -r -n 's/.*<(.*)>/\1/p')
+# headphones is the port in front of machine
 if [[ "$CURRENT_PORT" == "analog-output-headphones" ]]; then
-  pacmd set-sink-port 0 analog-output-lineout
+  pacmd set-sink-port 2 analog-output-lineout
   echo "🎧" > ~/.local/audio_device
 else
-  pacmd set-sink-port 0 analog-output-headphones
+  pacmd set-sink-port 2 analog-output-headphones
   echo "🔊" > ~/.local/audio_device
 fi
 
